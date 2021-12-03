d99751045c0c15fb9d3b3f76e4aa58c80a147624
tutasmaster <ruisims@gmail.com>
2021-12-03 11:10:19 +0000

    First commit (starts at Day3Part1)

---
+++ src/com/tutas/adventofcode/Main.java
@@ -0,0 +1,132 @@
+package com.tutas.adventofcode;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import static java.lang.Integer.compare;
+import static java.lang.Integer.parseInt;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try {
+            D3P1();
+        }catch (Exception e){
+
+        }
+    }
+
+    static void D3P1() throws FileNotFoundException, IOException{
+        int[] count = new int[12];
+        int totalLines = 0;
+        try (BufferedReader br = new BufferedReader(new FileReader("input.txt"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                for (int i = 0; i < line.length(); i++){
+                    count[i] += line.charAt(i) == '1' ? 1 : 0;
+                }
+
+                totalLines++;
+            }
+        }
+        String buffer = "";
+        String buffere = "";
+        for(int i = 0; i < count.length; i++){
+            int value = (totalLines / 2) > count[i] ? 1 : 0;
+            int iValue = value == 1 ? 0 : 1;
+            buffer = buffer + value;
+            buffere = buffere + iValue;
+        }
+        int finalGamma = parseInt(buffer,2);
+        int finalEpsilon = parseInt(buffere,2);
+        System.out.println(finalGamma);
+        System.out.println(finalEpsilon);
+        System.out.println(finalGamma*finalEpsilon);
+    }
+
+    static void D2P1() throws FileNotFoundException, IOException{
+        try (BufferedReader br = new BufferedReader(new FileReader("input.txt"))) {
+            String line;
+            Integer horizontal = 0, depth = 0;
+            while ((line = br.readLine()) != null) {
+                if(line.startsWith("u")){
+                    depth -= parseInt(line.split(" ")[1]);
+                }
+                if(line.startsWith("d")){
+                    depth += parseInt(line.split(" ")[1]);
+                }
+                if(line.startsWith("f")){
+                    horizontal += parseInt(line.split(" ")[1]);
+                }
+            }
+            System.out.println(horizontal);
+            System.out.println(depth);
+            System.out.println(horizontal*depth);
+        }
+    }
+
+    static void D2P2() throws FileNotFoundException, IOException{
+        try (BufferedReader br = new BufferedReader(new FileReader("input.txt"))) {
+            String line;
+            Integer horizontal = 0, depth = 0;
+            Integer aim = 0;
+            while ((line = br.readLine()) != null) {
+                if(line.startsWith("u")){
+                    aim += parseInt(line.split(" ")[1]);
+                }
+                if(line.startsWith("d")){
+                    aim -= parseInt(line.split(" ")[1]);
+                }
+                if(line.startsWith("f")){
+                    horizontal += parseInt(line.split(" ")[1]);
+                    depth -= aim * parseInt(line.split(" ")[1]);
+                }
+            }
+            System.out.println(horizontal);
+            System.out.println(depth);
+            System.out.println(horizontal*depth);
+        }
+    }
+
+    void D1P1() throws FileNotFoundException, IOException{
+        Integer i = 0;
+        Integer count = -1;
+        try (BufferedReader br = new BufferedReader(new FileReader("input.txt"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                if(i < parseInt(line)){
+                    count++;
+                }
+                i = parseInt(line);
+            }
+        }
+        System.out.println(count);
+    }
+
+    static Integer D1Wrap(Integer a){
+        return Math.abs(a % 3);
+    }
+
+    static void D1P2() throws FileNotFoundException, IOException{
+        Integer[] i = {0,0,0};
+        Integer i_buff = 2;
+        Integer count = -1;
+        try (BufferedReader br = new BufferedReader(new FileReader("input.txt"))) {
+            String line;
+            i[0] = parseInt(br.readLine());
+            i[1] = parseInt(br.readLine());
+            while ((line = br.readLine()) != null) {
+                Integer sum1 = i[0] + i[1] + i[2];
+                i[i_buff] = parseInt(line);
+                Integer sum2 = i[0] + i[1] + i[2];
+                if(sum1 < sum2){
+                    count++;
+                }
+                i_buff = D1Wrap(i_buff + 1);
+            }
+        }
+        System.out.println(count);
+    }
+}
